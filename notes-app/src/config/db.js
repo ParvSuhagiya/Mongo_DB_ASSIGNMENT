67fc5924b3a26ec562a1f28a19590c200dd28da4
ParvSuhagiya <parv.suhagiya.cg@gmail.com>
2026-04-18 09:41:38 +0530

    connected to mongo

--- notes-app/src/config/db.js
+++ notes-app/src/config/db.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017"
 
 async function connectDB(){
-    await mongoose.connect("mongodb://localhost:27017");
+    await mongoose.connect(mongoURI)
     console.log("Connected to DB...");
 }
 
